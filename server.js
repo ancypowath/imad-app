12f4f64df83c786772f6782371b8195b56a39e17
ancypowath <ancypowath@gmail.com>
2017-08-08 08:10:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article1',function(req,res){
+    res.send("this is article 1");
+});
+app.get('/article2',function(req,res){
+    res.send("this is article 2");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
